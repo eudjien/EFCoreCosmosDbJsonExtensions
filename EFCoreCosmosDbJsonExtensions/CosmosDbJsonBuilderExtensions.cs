a9cf8edebc906c42f485c49cf085c5891f3b96a4
Evgeny Pinchuk <evgnpn@gmail.com>
2021-03-08 13:06:23 +0300

    Updated to .NET 5.0, updated README.md

--- EFCoreCosmosDbJsonExtensions/CosmosDbJsonBuilderExtensions.cs
+++ EFCoreCosmosDbJsonExtensions/CosmosDbJsonBuilderExtensions.cs
@@ -5,20 +5,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace Evgnpn.EntityFrameworkCore.CosmosDb.Extensions
+namespace EFCoreCosmosDbJsonExtensions
 {
     public static class CosmosDbJsonBuilderExtensions
     {
         public static void ToJsonProperty<T>(this PropertyBuilder<T> builder) where T : class
         {
-            builder.ToJsonProperty(FirstLetterToLowerCase(builder.Metadata.Name));
+            builder.ToJsonProperty(FirstCharToLowerCase(builder.Metadata.Name));
         }
 
         public static void ToJsonProperties<T>(this EntityTypeBuilder<T> builder) where T : class
         {
             foreach (var property in GetProperties(builder))
             {
-                builder.Property(property.Name).ToJsonProperty(FirstLetterToLowerCase(property.Name));
+                builder.Property(property.Name).ToJsonProperty(FirstCharToLowerCase(property.Name));
             }
         }
 
@@ -26,10 +26,10 @@ namespace Evgnpn.EntityFrameworkCore.CosmosDb.Extensions
             where T : class
             where D : class
         {
-            builder.ToJsonProperty(FirstLetterToLowerCase(builder.Metadata.GetNavigation(false).Name));
+            builder.ToJsonProperty(FirstCharToLowerCase(builder.Metadata.GetNavigation(false).Name));
             foreach (var property in GetProperties(builder))
             {
-                builder.Property(property.Name).ToJsonProperty(FirstLetterToLowerCase(property.Name));
+                builder.Property(property.Name).ToJsonProperty(FirstCharToLowerCase(property.Name));
             }
         }
 
@@ -46,21 +46,21 @@ namespace Evgnpn.EntityFrameworkCore.CosmosDb.Extensions
             return typeof(D).GetProperties().Where(a => a.CanWrite && a.CanRead);
         }
 
-        private static string FirstLetterToLowerCase(string input)
+        private static string FirstCharToLowerCase(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
             {
                 throw new ArgumentNullException(nameof(input));
             }
 
-            var firstLetter = input.Substring(0, 1).ToLower();
+            var firstChar = input.Substring(0, 1).ToLower();
 
             if (input.Length == 1)
             {
-                return firstLetter;
+                return firstChar;
             }
 
-            return firstLetter + new string(input.Skip(1).ToArray());
+            return firstChar + new string(input.Skip(1).ToArray());
         }
     }
 }
